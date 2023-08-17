c9fccf202561a1c42ce22add6c18b8c2523e9506
Akshay Verma <47255769+akshayverma1697@users.noreply.github.com>
2023-08-16 23:37:06 -0500

    Created Results Table Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./Components/Header/Header";
+import ResultsTable from "./Components/ResultsTable/ResultsTable";
 import UserInput from "./Components/UserInput/UserInput";
 
 function App() {
@@ -32,32 +33,10 @@ function App() {
   return (
     <div>
       <Header/>
-
       <UserInput/>
-
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-
-      <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
+      <ResultsTable/>
     </div>
   );
 }
